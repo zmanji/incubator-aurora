6a79ff1ada57aead3c27b61db6d321f2bae1e4f9
Zameer Manji <zmanji@twitter.com>
2014-10-06 12:02:27 -0700

    Add a comment about timeout.

--- build-support/jenkins/build.sh
+++ build-support/jenkins/build.sh
@@ -21,6 +21,8 @@ date
 ./gradlew -Pq clean build --stacktrace
 
 # Run all Python tests
+# Setting the timeout value to 1 minute ensures package fetches from PyPI do not
+# fail on Apache Jenkins.
 ./pants build --timeout=60 src/test/python:all -vxs
 
 # Run Python style checks
