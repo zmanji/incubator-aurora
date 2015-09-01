a7705adcdf950d83fef2162822951619bfd861b7
Zameer Manji <zmanji@apache.org>
2015-08-31 17:31:44 -0700

    WIP.

--- src/main/java/org/apache/aurora/scheduler/http/api/security/HttpSecurityModule.java
+++ src/main/java/org/apache/aurora/scheduler/http/api/security/HttpSecurityModule.java
@@ -14,6 +14,7 @@
 package org.apache.aurora.scheduler.http.api.security;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.Set;
 
@@ -43,6 +44,7 @@ import org.apache.aurora.scheduler.thrift.aop.AnnotatedAuroraAdmin;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.guice.aop.ShiroAopModule;
 import org.apache.shiro.guice.web.ShiroWebModule;
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.subject.Subject;
 
 import static java.util.Objects.requireNonNull;
@@ -133,6 +135,11 @@ public class HttpSecurityModule extends ServletModule {
     } else {
       doConfigureServlets();
     }
+  }
+
+  @Provides
+  public Collection<Realm> provideRealmCollection(Set<Realm> realms) {
+    return realms;
   }
 
   private void doConfigureServlets() {
